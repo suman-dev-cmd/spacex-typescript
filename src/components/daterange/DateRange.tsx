8998eebd02fcdc24d21d1b77cb2e68d350522090
suman-dev-cmd <sumanj@capitalnumbers.com>
2021-10-22 18:14:47 +0530

    fixed

--- src/components/daterange/DateRange.tsx
+++ src/components/daterange/DateRange.tsx
@@ -1,11 +1,4 @@
-import React, { useState } from "react";
-import DateRangePicker from "react-daterange-picker";
-import "react-daterange-picker/dist/css/react-calendar.css";
 import { DateRangePickerComponent } from "@syncfusion/ej2-react-calendars";
-import * as Moment from "moment";
-import { extendMoment } from "moment-range";
-
-const moment = extendMoment(Moment);
 interface DateProps {
  
   onSelect: (value: any) => any;
