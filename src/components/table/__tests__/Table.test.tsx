6efdf555a2f8c9b7802bd50d962cd942c3741e34
suman-dev-cmd <sumanj@capitalnumbers.com>
2021-10-29 11:37:44 +0530

    test

---
+++ src/components/table/__tests__/Table.test.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import TableComponent from "../TableComponent";
+
+
+
+describe("<TableComponent />", () => {
+  test("should display a blank login form, with remember me checked by default", async () => {
+    // ???
+  });
+});
